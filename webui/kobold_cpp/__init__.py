cec6241cc97e6fb57ac7838cc72d3a0b761f1892
thesavant42 <jbrashars@gmail.com>
2024-12-16 22:51:14 -0800

    Update __init__.py

--- webui/kobold_cpp/__init__.py
+++ webui/kobold_cpp/__init__.py
@@ -23,7 +23,7 @@ def start_server(model, host="localhost", port=8000, n_gpu_layers=0, n_ctx=2048)
     
     base_url = f"http://{host}:{port}/api"
     cmd = ["koboldcpp.exe",f"--model={model}",f"--host={host}",f"--port={port}",f"--gpulayers={n_gpu_layers}",
-           f"--contextsize={n_ctx}", "--skiplauncher", "--multiuser", "--usecublas", "--quiet"]
+           f"--contextsize={n_ctx}", "--skiplauncher", "--multiuser", "--usecublas"]
     print(f"{cmd=}")
     process = subprocess.Popen(cmd, cwd=CWD)
     for i in range(30): # wait for server to start up
